7c1bd03a67838689ef79f88078470fac504b42cd
Kay <kayxn23@gmail.com>
2018-09-20 13:34:06 -0700

    fixed some verbs in the route file"

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,17 +24,25 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description]) #instantiate a new task
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the task in the list
+    else # save failed :(
+      render :new # show the new task form view again
+    end
   end
 
-  def edit
+  def edit #this method gives the user a form 
+    @task = Task.find(params[:id].to_i)
   end
 
   def update
   end
 
   def destroy
-  end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root "tasks#index"
   get '/tasks', to: 'tasks#index', as: 'tasks' # as: allows us to name a path like tasks_path
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  get '/tasks', to: 'tasks#create'
+  post '/tasks', to: 'tasks#create'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  get '/tasks/:id', to: 'tasks#update'
+  patch '/tasks/:id', to: 'tasks#update'
+  put '/tasks/:id', to: 'tasks#update'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
-  get '/tasks/:id', to: 'tasks#destroy'
+  delete '/tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#any path that has :id will need a param , so you need to tell it what id to use 
